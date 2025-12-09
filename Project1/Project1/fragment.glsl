6e1f3b32e0ae2652ee6fa36bc1359a1f194550cc
heonilha <heonil0304@gmail.com>
2025-12-09 16:08:16 +0900

    코드 수정

--- Project1/Project1/fragment.glsl
+++ Project1/Project1/fragment.glsl
@@ -1,29 +1,7 @@
 #version 330 core
-
-in vec3 FragPos;
-in vec3 Normal;
-
 out vec4 FragColor;
-
 uniform vec3 objectColor;
-uniform vec3 lightPos;
-
 void main()
 {
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-
-    // Ambient (기본 밝기)
-    vec3 ambient = objectColor * 0.30;
-
-    // Diffuse
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = objectColor * diff * 1.5;  // 밝기를 높임
-
-    // Distance attenuation (최소한 적용)
-    float dist = length(lightPos - FragPos);
-    float attenuation = 1.0 / (1.0 + 0.05 * dist + 0.02 * dist * dist);
-
-    vec3 result = (ambient + diffuse) * attenuation;
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(objectColor, 1.0);
 }
--- Project1/Project1/vertex.glsl
+++ Project1/Project1/vertex.glsl
@@ -1,22 +1,11 @@
 #version 330 core
-
-layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec3 aNormal;
-
-out vec3 FragPos;
-out vec3 Normal;
-
+layout (location = 0) in vec3 aPos;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 uniform float clipSign;
-
 void main()
 {
-    FragPos = vec3(model * vec4(aPos, 1.0));
-    Normal = mat3(transpose(inverse(model))) * aNormal;
-
-    float localY = FragPos.y - model[3].y;
-    gl_ClipDistance[0] = clipSign * localY;
-    gl_Position = projection * view * vec4(FragPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_ClipDistance[0] = clipSign * aPos.y;
 }
