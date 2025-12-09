895c3bbe0f1dc35fe87eb5107131476723b83127
heonilha <heonil0304@gmail.com>
2025-12-09 16:13:01 +0900

    Add localized lighting falloff around player

--- Project1/Project1/fragment.glsl
+++ Project1/Project1/fragment.glsl
@@ -1,7 +1,19 @@
 #version 330 core
 out vec4 FragColor;
+
+in vec3 FragPos;
+
 uniform vec3 objectColor;
+uniform vec3 lightPos;
+uniform float lightRadius;
+
 void main()
 {
-    FragColor = vec4(objectColor, 1.0);
+    float distanceToLight = length(FragPos - lightPos);
+    float radius = max(lightRadius, 0.001);
+
+    float falloff = clamp(1.0 - distanceToLight / radius, 0.0, 1.0);
+    float intensity = mix(0.15, 1.0, falloff * falloff);
+
+    FragColor = vec4(objectColor * intensity, 1.0);
 }
